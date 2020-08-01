96bac9f21d0b4f0790e223bdfb90a33a76285447
antonio63j <antonio63j@gmail.com>
2020-08-01 11:50:04 +0200

    cambios environmet.ts de produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     // urlEndPoint: 'https://aflcv-back.herokuapp.com',
-    urlEndPoint: 'http://ec2-18-157-252-11.eu-central-1.compute.amazonaws.com:8080'
+    urlEndPoint: 'https://ec2-18-157-252-11.eu-central-1.compute.amazonaws.com:8080'
 };
